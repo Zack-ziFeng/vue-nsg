bc4271d5428858ad15dc5ca17642b937678fbc21
e <1282229539@qq.com>
2019-01-08 15:36:14 +0800

    axios安装

--- src/components/page/category.vue
+++ src/components/page/category.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>category</h1>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: null
+    }
+  },
+  mounted() {
+    this.axios.get('https://www.nanshig.com/mobile/index.php', {
+      params: {
+        act: 'goods_class'
+      }
+    }).then(res=>{
+      this.data = res;
+      console.log(this.data);
+    }).catch(err=>{
+      console.log(err);
+    });
+  }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import Vueaxios from 'vue-axios'
 
 // 引入mint-ui组件和css
 import MintUI from 'mint-ui'
@@ -10,6 +12,7 @@ import 'mint-ui/lib/style.css'
 
 // use
 Vue.use(MintUI)
+Vue.use(Vueaxios, axios)
 
 Vue.config.productionTip = false
 
