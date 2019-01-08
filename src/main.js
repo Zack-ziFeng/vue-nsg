425c2ccac5368c7891bc8737d66871c4dbf4c8e4
ljc <846836199@qq.com>
2019-01-08 11:19:40 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -4,21 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// 引入element-ui组件和css
-import ElementUi from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
-// 引入vuex
-import Vuex from 'vuex'
-import store from './vuex/store'
-
 // 引入mint-ui组件和css
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 // use
-Vue.use(ElementUi)
-Vue.use(Vuex)
 Vue.use(MintUI)
 
 Vue.config.productionTip = false
@@ -27,7 +17,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
